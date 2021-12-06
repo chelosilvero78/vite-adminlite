afac9aaeef7e2527ed170e2b66f7ef124bf720c2
chelosilvero78 <marcesilvero@gmail.com>
2021-12-06 08:44:06 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//import logo from './logo.svg'
+import Header from './components/Header'
+import Aside from './components/Aside'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import './App.css'
+function App() {
+  return (
+    <>
+      <Header />
+      <Aside />
+      <Content />
+      <Footer />
+    </>
+  )
+}
+
+export default App
